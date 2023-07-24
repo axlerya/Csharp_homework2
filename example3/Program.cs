9bd69bd016a2309ea1874b306f6524abd5089ef7
axlerya <white_cat_02@mail.ru>
2023-07-25 04:42:38 +0500

    add example3

---
+++ example3/Program.cs
@@ -0,0 +1,11 @@
+﻿void NumberWeekDay(int numberDay)
+{
+    if (numberDay == 6 | numberDay == 7) Console.WriteLine("да");
+    else Console.WriteLine("нет");
+}
+
+Console.WriteLine("Введите число - день недели");
+int numberInput = Convert.ToInt32(Console.ReadLine());
+
+if (numberInput > 8) Console.WriteLine("Введите день недили правильно");
+else NumberWeekDay(numberInput);

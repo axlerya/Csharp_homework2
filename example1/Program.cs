07ff0343647497cdcd39e5cf454f44ef7370bad9
axlerya <white_cat_02@mail.ru>
2023-07-25 02:35:49 +0500

    add example1

---
+++ example1/Program.cs
@@ -0,0 +1,18 @@
+﻿void Number(int number)
+{   
+    double positionTwo = 0;
+    if (number/100 > 0) 
+    {
+        positionTwo = number / 10 % 10;
+        Console.WriteLine($"Вторая чифра числа = {positionTwo}");
+    }
+
+    else
+    {
+        Console.WriteLine($"Введите трех значное число");
+    }
+}
+
+﻿Console.WriteLine("Введите первое число");
+int number = Convert.ToInt32(Console.ReadLine());
+Number(number);

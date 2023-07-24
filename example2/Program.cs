dba405fb188f282b9a41e5f9a7f6fae55c110f51
axlerya <white_cat_02@mail.ru>
2023-07-25 04:19:24 +0500

    add example2

---
+++ example2/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] GetIntArray(int num)
+{
+    List<int> listOfInts = new List<int>();
+    while(num > 0)
+    {
+        listOfInts.Add(num % 10);
+        num = num / 10;
+    }
+    listOfInts.Reverse();
+    int[] arrayNumbers = listOfInts.ToArray();
+    return arrayNumbers;
+}
+
+
+Console.WriteLine("Введите число");
+int numberInput = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = GetIntArray(numberInput);
+
+if (numberInput%100 > 0)
+{
+    Console.WriteLine($"Третья цифра числа = {arrayNumbers[2]}");
+}
+
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
